89c3fcdc0a31a43bcc5ccf99c77fff0b15fdfef2
Khan Ataur Rahman <khanataur.jp@gmail.com>
2024-12-03 22:17:43 +0600

    Employee List Get and Post API done...

---
+++ EmployeePortal/Controllers/EmployeeController.cs
@@ -0,0 +1,33 @@
+﻿using EmployeePortal.Models;
+using EmployeePortal.Repository;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace EmployeePortal.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class EmployeeController : ControllerBase
+    {
+        private readonly EmployeeRepository _employeeRepository;
+        public EmployeeController(EmployeeRepository employeeRepository)
+        {
+            _employeeRepository = employeeRepository;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult> EmployeeList()
+        {
+            var allEmployee = await _employeeRepository.GetAllEmployees();
+            return Ok(allEmployee);
+        }
+
+        [HttpPost]
+        public async Task<ActionResult> AddEmployee(Employee objEmployee)
+        {
+            await _employeeRepository.SaveEmployee(objEmployee);
+            return Ok(objEmployee);
+        }
+
+    }
+}
